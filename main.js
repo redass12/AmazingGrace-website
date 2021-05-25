78bfb918949baaf1b559ede60be15e09440fd2d7
redass12 <redataraoui1@gmail.com>
2021-05-25 17:37:32 +0200

    1:making read more functional 2:fix the navigation in the mobile phone

--- main.js
+++ main.js
@@ -2,10 +2,33 @@
 
 const nav = document.querySelector("nav");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 30) {
-    nav.classList.add("nav-animate");
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 30) {
+//     nav.classList.add("nav-animate");
+//   } else {
+//     nav.classList.remove("nav-animate");
+//   }
+// });
+
+// Read more button
+
+const readMoreBtn = document.querySelector("#read-more");
+
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", () => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Lire Plus") {
+    readMoreBtn.innerText = "Lire Moin";
   } else {
-    nav.classList.remove("nav-animate");
+    readMoreBtn.innerText = "Lire Plus";
   }
+});
+
+// Nav bar button
+
+let menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  nav.classList.toggle("active");
 });
